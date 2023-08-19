f94b237b27d1e785b1c81df7692731f611ee1007
UPIMAN73 <joshcalzadi@gmail.com>
2023-08-19 18:21:38 -0400

    JSON Parser Needs Fix

--- Map/map.go
+++ Map/map.go
@@ -11,19 +11,21 @@ type Zone struct {
 	Name        string `json:"name"`
 	Description string `json:"description"`
 	// Position Connections to other Zones (this includes other Regions & Worlds as well)
-	Connections []Position
+	Connections []Position	`json:"connections"`
+	// Zone Environment Type
+	BattleZone bool		`json:"battlezone"`
 }
 
 // Region Structure
 type Region struct {
 	Name        string          `json:"name"`
 	Description string          `json:"description"`
-	zones       map[uint16]Zone // Zones
+	zones       map[uint16]Zone `json:"zones"`// Zones	
 }
 
 // World Structure
 type World struct {
 	Name        string            `json:"name"`
 	Description string            `json:"description"`
-	Regions     map[uint16]Region // Regions
+	Regions     map[uint16]Region `json:"regions"`// Regions
 }
--- test.go
+++ test.go
@@ -4,6 +4,7 @@ import (
 	"encoding/json"
 	"fmt"
 	"os"
+	// "Map"
 )
 
 type Position struct {
